924925775fbd7bee4fc036dd2000488c5c873786
karaciubamikhail <karaciubamikhail@gmail.com>
2024-04-24 14:59:06 +0300

    Done

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,34 @@
+import getCharecter from '../app';
+
+test('tests', () => {
+  const character = {
+    name: "Лучник",
+    type: "Bowman",
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: "Двойной выстрел",
+        icon: "http://...",
+        description: "Двойной выстрел наносит двойной урон",
+      },
+      {
+        id: 9,
+        name: "Нокаутирующий удар",
+        icon: "http://...",
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  const findID = getCharecter(character,8);
+  const result = {
+    id: 9,
+    name: "Нокаутирующий удар",
+    icon: "http://...",
+    description: "Описание засекречено",
+  }
+  expect(findID).toBe(result);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function getCharecter(obg, ...data) {
+    const datas = obg.special.find((dat)=>{
+      return dat.id === data[0];
+    })
+    if(datas.description == undefined){
+      datas['description'] = "Описание засекречино";
+    }
+    return datas;
+  }
